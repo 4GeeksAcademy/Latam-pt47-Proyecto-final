f0de67f348f59cf4aa1769c85f44f73b480615eb
DanielGG207 <cocorroquiasprogamer@gmail.com>
2025-05-28 21:45:03 +0000

    [render-danielgg]

--- src/app.py
+++ src/app.py
@@ -28,7 +28,7 @@ from flask_mail import Mail, Message
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), '../public/')
+    os.path.realpath(__file__)), '../dist/')
 app = Flask(__name__)
 
 app.config.update(dict(
