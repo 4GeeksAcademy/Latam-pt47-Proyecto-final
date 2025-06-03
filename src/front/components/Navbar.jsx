cefcbb588192b0a09a4e51ff5c22091497655e60
Santiago <126203646+SMartinGonzalez@users.noreply.github.com>
2025-06-03 14:51:12 +0000

    [logo navbar]

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
-import logo from "../../assets/Logo-GuardianUrbano.png";
+import logo from "src/assets/Logo-GuardianUrbano.png";
 
 
 export const Navbar = () => {
